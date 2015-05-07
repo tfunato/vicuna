1428d200ace45de5a10998588e82042db49552df
tfunato <tfunato@healthcare.ne.jp>
2015-05-07 15:41:28 +0900

    add initial

---
+++ src/main/java/jp/canetrash/vicuna/Application.java
@@ -0,0 +1,21 @@
+package jp.canetrash.vicuna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+/**
+ * vicuna application
+ *
+ */
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class Application {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext ctx = SpringApplication.run(
+				Application.class, args);
+	}
+}
---
+++ src/main/java/jp/canetrash/vicuna/controller/Index.java
@@ -0,0 +1,15 @@
+package jp.canetrash.vicuna.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class Index {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String index() {
+		return "Hello World!";
+	}
+}
