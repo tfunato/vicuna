1c1a71dd3229dfa059c3936a50ac21bcb1e786d1
tfunato <tfunato@healthcare.ne.jp>
2015-05-14 17:55:58 +0900

    add html template

--- src/main/java/jp/canetrash/vicuna/web/controller/Index.java
+++ src/main/java/jp/canetrash/vicuna/web/controller/Index.java
@@ -5,12 +5,15 @@ import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * @author tfunato
+ *
+ */
 @Controller
 public class Index {
 
 	@RequestMapping("/")
 	public String index(Map<String, Object> model) {
-        model.put("message", "Hello world");
-		return "index";
+		return "about";
 	}
 }
