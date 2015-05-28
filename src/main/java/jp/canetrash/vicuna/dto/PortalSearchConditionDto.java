058863ce8bed01c2b55bed6d341c588621b39899
tfunato <tfunato@healthcare.ne.jp>
2015-05-28 19:00:08 +0900

    add portal search, in progress

---
+++ src/main/java/jp/canetrash/vicuna/dto/PortalSearchConditionDto.java
@@ -0,0 +1,46 @@
+package jp.canetrash.vicuna.dto;
+
+/**
+ * @author tfunato
+ *
+ */
+public class PortalSearchConditionDto {
+
+	private String neLat;
+	private String swLat;
+	private String neLng;
+	private String swLng;
+
+	public String getNeLat() {
+		return neLat;
+	}
+
+	public void setNeLat(String neLat) {
+		this.neLat = neLat;
+	}
+
+	public String getSwLat() {
+		return swLat;
+	}
+
+	public void setSwLat(String swLat) {
+		this.swLat = swLat;
+	}
+
+	public String getNeLng() {
+		return neLng;
+	}
+
+	public void setNeLng(String neLng) {
+		this.neLng = neLng;
+	}
+
+	public String getSwLng() {
+		return swLng;
+	}
+
+	public void setSwLng(String swLng) {
+		this.swLng = swLng;
+	}
+
+}
---
+++ src/main/java/jp/canetrash/vicuna/web/controller/Portals.java
@@ -0,0 +1,17 @@
+package jp.canetrash.vicuna.web.controller;
+
+import java.util.List;
+
+import jp.canetrash.vicuna.dto.PortalDto;
+import jp.canetrash.vicuna.dto.PortalSearchConditionDto;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Portals {
+
+	public List<PortalDto> portals(PortalSearchConditionDto condition) {
+
+		return null;
+	}
+}
