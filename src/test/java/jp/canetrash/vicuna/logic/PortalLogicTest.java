4c50b1a3b7d8a12934f068fd1ee6d13750f56107
tfunato <tfunato@gmail.com>
2015-06-27 11:03:54 +0900

    modify unit test in progress

--- src/test/java/jp/canetrash/vicuna/logic/PortalLogicTest.java
+++ src/test/java/jp/canetrash/vicuna/logic/PortalLogicTest.java
@@ -1,14 +1,24 @@
 package jp.canetrash.vicuna.logic;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+import jp.canetrash.vicuna.Application;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
 public class PortalLogicTest {
+
+	@Autowired
+	private PortalLogic portalLogic;
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		assertNotNull(portalLogic);
 	}
 
 }
